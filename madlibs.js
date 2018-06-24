f717db61d468e2d3a2ff7e97bc696f483e153f8d
SIANSS <acsian7@gmail.com>
2018-06-24 14:42:28 +0530

    The Home push of Freecodecamp

---
+++ madlibs.js
@@ -0,0 +1,10 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "";
+  result += "Hola "+myNoun+" mi "+myAdjective+" la ponso "+myVerb+" reco "+myAdverb;
+  // Your code above this line
+  console.log(result);
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
